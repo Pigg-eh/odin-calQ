4a68daea48c80ab75cedcdebfefdbf5074f920fc
Hunter <ep.hunter23@gmail.com>
2023-10-13 17:26:59 -0700

    feat Add initial css
    Add visual aid to web app

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+function showDisplay (a, b, operator){
+    
+}
+
+function addNumbers (a, b){
+    sum = a + b
+}
+
+function subtractNumbers(a, b){
+
+}
+
+function multiplyNumbers(a, b){
+
+}
+
+function divideNumbers(a, b){
+
+}
