5b0ef51b91e12532af22e065d990c5ebff9256b7
Hunter <ep.hunter23@gmail.com>
2023-11-05 17:05:38 -0800

    feat: Add button interface
      Edit dimensions to make UI look presentable to the user

--- scripts.js
+++ scripts.js
@@ -1,19 +1,63 @@
+/*DOM MANIPULATION */
+let number = ['+','-', '/', '*', ]
 
-
-let buttonArray = function () {
+makeBtnArray()
+function makeBtnArray  () {
     //potentially need to use object to add classes
-    let number = ['+','-', '/', '*', ]
-
-    for (let i = 1; i<10; i++){
+    for (i = 1; i<10; i++){
         number.splice ((4+i), 0, i)
     }
     number.push(0)
     number.push('=')
 
     console.log (number)
+    
 }
-buttonArray()
 
+insertBtn()
+function insertBtn () {
+    let buttonQuery = document.querySelector('div.buttonsContainer')
+    for (i=0; i<number.length; i++){
+        makeBtnArray[i] = document.createElement('button')
+        buttonQuery.appendChild(makeBtnArray[i])
+    }
+    addCalcBtnClass()
+}
+
+
+function addCalcBtnClass() {
+    let btnClasses = document.querySelectorAll('div.buttonsContainer>button')
+
+    btnClasses.forEach((btnClass) => {
+        btnClass.classList.add('calcBtn')
+    })
+
+    insertText ()
+}
+
+function insertText (x=0) {
+console.log (number.length)
+
+    let buttonTexts = document.querySelectorAll('.calcBtn')
+
+    buttonTexts.forEach((buttonText) => {
+        buttonText.textContent = number[x];
+        x++;
+    })
+}
+
+/* */
+let xNumber
+let yNumber
+let operator
+
+function operate (x, y, sign){
+    return `${x} ${sign} ${y}`
+}
+
+
+
+/*CALCULATOR OPERATIONS */
 
 function addNumbers (a, b){
     return a + b
